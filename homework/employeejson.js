4a490b85a3145750604be2ddbbcfc877d9cd59c2
Jessica Hart <jessicahart@Jessicas-MBP.attlocal.net>
2020-04-23 23:47:04 -0700

    worked on employee hw and todo list that has not converted to json

--- homework/employeejson.js
+++ homework/employeejson.js
@@ -1,7 +1,8 @@
 //I am a small business owner with 3 employees and I need to let go 1 becasue of covad 19. I will create 3 objects with nested informaton on each employee's partner 
 
-`[
-{
+
+let myEmployees = {
+
     "name": "Lena",
     "age": 30,
     "productivityRating": 4,
@@ -10,7 +11,8 @@
         "age": 31,
         "useful": true,
         "teamplayer": false
-    }]
+    }],
+    
     "name": "Giovanni",
     "age": 30,
     "productivityRating": 5,
@@ -19,7 +21,8 @@
         "age": 22,
         "useful": false,
         "teamplayer": true
-    }]
+    }],
+    
     "name": "Daisy",
     "age": 35,
     "productivityRating": 5,
@@ -29,10 +32,11 @@
         "useful": true,
         "teamplayer": false
     }]
+};
+    
 
-}]`
 
 // this will return a string becasue we used JSON.parse, and turn it into a JS object.
 
 // [1].partner should return information about Giovanni's partner.
-console.log(JSON.parse(employeesjson)[1].partner)
+// console.log(JSON.parse(employeesjson)[1].partner)

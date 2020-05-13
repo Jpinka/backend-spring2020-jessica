6b89bd7e8243961d57830fc27bb84604867776a4
Jessica Hart <jessicahart@Jessicas-MBP.attlocal.net>
2020-05-13 09:28:13 -0700

    adding first draft of todo project

---
+++ practice/week5/javascript/deepvsshallowcopy.js
@@ -0,0 +1,23 @@
+// Deep Copy
+let variable1 = 100;
+
+let variable2 = variable1;
+
+let variable3 = variable2 + 10; 
+
+console.log(variable1);
+console.log(variable3);
+
+// Shallow Copy, really means a new "reference" to "old" data.
+let myObject = {
+    info1: "something",
+    info2: 1000
+};
+
+let myObject2 = myObject;
+
+let myObject3 = myObject2;
+
+myObject3.info2 = 2000;
+
+console.log(myObject.info2);
